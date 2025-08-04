53cd839518f1efa4444e196ce721e28e5f3c6552
oguzhanoozer <oguzoozer@gmail.com>
2025-08-04 13:07:04 +0300

    - Init projects files added to project.

---
+++ User Management App/User Management App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  User Management App
+//
+//  Created by oguzhan on 4.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ User Management App/User Management App/User_Management_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  User_Management_AppApp.swift
+//  User Management App
+//
+//  Created by oguzhan on 4.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct User_Management_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
